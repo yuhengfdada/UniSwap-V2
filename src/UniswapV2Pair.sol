8d3f64394caeb43aab96a6fbf2e6fa1b7bd85ee3
yuhengfdada <877751954@qq.com>
2025-01-19 15:26:47 +0800

    UniswapV2Pair

---
+++ src/UniswapV2Pair.sol
@@ -0,0 +1,139 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC20} from "solmate/src/tokens/ERC20.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import {UQ112x112} from "../lib/UQ112x112.sol";
+
+import {Test, console} from "forge-std/Test.sol";
+
+contract UniswapV2Pair is ERC20 {
+    address token0;
+    address token1;
+
+    // gas savings
+    uint112 public reserve0;
+    uint112 public reserve1;
+    uint32 blockTimestampLast;
+
+    uint256 price0CumulativeLast;
+    uint256 price1CumulativeLast;
+
+    uint256 MINIMUM_LIQUIDITY = 1e3;
+
+    event Mint(address indexed sender, uint256 amount0, uint256 amount1);
+    event Burn(address indexed sender, uint256 amount0, uint256 amount1);
+    event Swap(address indexed sender, uint256 amount0Out, uint256 amount1Out, address indexed to);
+
+    constructor(address _token0, address _token1) ERC20("Uniswap-V2", "UNIV2", 18) {
+        token0 = _token0;
+        token1 = _token1;
+    }
+
+    function getReserves() public view returns (uint112, uint112) {
+        return (reserve0, reserve1);
+    }
+
+    function _updateReserves(uint256 balance0, uint256 balance1, uint112 _reserve0, uint112 _reserve1) private {
+        uint32 blockTimestamp = uint32(block.timestamp); // gas saving
+
+        unchecked {
+            // can overflow but subtraction returns the correct result.
+            // e.g. 0 - 1 = INT_MAX
+            uint32 timePassedSinceLast = blockTimestamp - blockTimestampLast;
+            if (timePassedSinceLast > 0 && _reserve0 != 0 && _reserve1 != 0) {
+                // * never overflows (224*32 <= 256); += can overflow but subtraction (performed by user) returns the correct result.
+                price0CumulativeLast +=
+                    uint256(UQ112x112.uqdiv(UQ112x112.encode(_reserve1), _reserve0)) * timePassedSinceLast;
+                price1CumulativeLast +=
+                    uint256(UQ112x112.uqdiv(UQ112x112.encode(_reserve0), _reserve1)) * timePassedSinceLast;
+            }
+        }
+
+        reserve0 = uint112(balance0);
+        reserve1 = uint112(balance1);
+        blockTimestampLast = blockTimestamp;
+    }
+
+    // Some tokens might not give a return value for transfer() (e.g. USDT)
+    function _safeTransfer(address token, address to, uint256 amount) private {
+        (bool success, bytes memory data) = token.call(abi.encodeWithSignature("transfer(address,uint256)", to, amount));
+        if (!success || data.length != 0 && !abi.decode(data, (bool))) {
+            revert("transfer failed");
+        }
+    }
+
+    // No token amount is passed in. Users have to transfer tokens to the Pair in the same transaction.
+    function mint() public {
+        // gas savings
+        (uint112 _reserve0, uint112 _reserve1) = getReserves();
+
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+
+        uint256 amount0 = balance0 - _reserve0;
+        uint256 amount1 = balance1 - _reserve1;
+
+        require(amount0 > 0 && amount1 > 0);
+
+        uint256 liquidity;
+
+        if (totalSupply == 0) {
+            liquidity = Math.sqrt(amount0 * amount1) - MINIMUM_LIQUIDITY;
+            _mint(address(0), MINIMUM_LIQUIDITY);
+        } else {
+            liquidity = Math.min(amount0 * totalSupply / _reserve0, amount1 * totalSupply / _reserve1);
+        }
+
+        _mint(msg.sender, liquidity);
+        _updateReserves(balance0, balance1, _reserve0, _reserve1);
+
+        emit Mint(msg.sender, amount0, amount1);
+    }
+
+    // No burn amount is passed in. Users have to transfer LP Token to the Pair in the same transaction.
+    function burn() public {
+        // gas savings
+        (uint112 _reserve0, uint112 _reserve1) = getReserves();
+
+        uint256 burnAmount = balanceOf[address(this)];
+
+        uint256 amount0 = burnAmount * _reserve0 / totalSupply;
+        uint256 amount1 = burnAmount * _reserve1 / totalSupply;
+
+        require(amount0 > 0 && amount1 > 0);
+
+        _safeTransfer(token0, msg.sender, amount0);
+        _safeTransfer(token1, msg.sender, amount1);
+
+        _burn(address(this), burnAmount);
+
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+        _updateReserves(balance0, balance1, _reserve0, _reserve1);
+
+        emit Burn(msg.sender, amount0, amount1);
+    }
+
+    function swap(uint256 amount0Out, uint256 amount1Out, address to) public {
+        require(amount0Out > 0 || amount1Out > 0, "Out amount should be greater than 0");
+
+        // gas savings
+        (uint112 _reserve0, uint112 _reserve1) = getReserves();
+        require(amount0Out <= _reserve0 && amount1Out <= _reserve1, "No enough reserve");
+
+        // The user should have sent the tokens in before calling swap(). Below calculates the expected token amount after the transaction.
+        uint256 balance0 = IERC20(token0).balanceOf(address(this)) - amount0Out;
+        uint256 balance1 = IERC20(token1).balanceOf(address(this)) - amount1Out;
+        // need to cast _reserve to uint256. Otherwise easy to overflow uint112
+        require(balance0 * balance1 >= uint256(_reserve0) * uint256(_reserve1), "Invalid K");
+
+        _updateReserves(balance0, balance1, _reserve0, _reserve1);
+
+        if (amount0Out > 0) _safeTransfer(token0, to, amount0Out);
+        if (amount1Out > 0) _safeTransfer(token1, to, amount1Out);
+
+        emit Swap(msg.sender, amount0Out, amount1Out, to);
+    }
+}
---
+++ test/UniswapV2Pair.t.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Test, console} from "forge-std/Test.sol";
+import {UniswapV2Pair} from "../src/UniswapV2Pair.sol";
+import {Token} from "../src/Token.sol";
+
+contract TestUniswapV2Pair is Test {
+    Token token1;
+    Token token2;
+    UniswapV2Pair pair;
+
+    uint256 MINIMUM_LIQUIDITY = 1e3;
+
+    event Swap(address indexed sender, uint256 amount0Out, uint256 amount1Out, address indexed to);
+
+    function setUp() public {
+        token1 = new Token("TokenX", "X", 100 ether);
+        token2 = new Token("TokenY", "Y", 100 ether);
+        pair = new UniswapV2Pair(address(token1), address(token2));
+    }
+
+    function assertReserves(uint256 token0Reserve, uint256 token1Reserve) public view {
+        assertEq(token0Reserve, pair.reserve0());
+        assertEq(token1Reserve, pair.reserve1());
+    }
+
+    function testMint() public {
+        token1.transfer(address(pair), 10 ether);
+        token2.transfer(address(pair), 10 ether);
+        pair.mint();
+        assertEq(pair.balanceOf(address(this)), 10 ether - MINIMUM_LIQUIDITY);
+        assertReserves(10 ether, 10 ether);
+        assertEq(pair.totalSupply(), 10 ether);
+    }
+
+    function testMintUnbalanced() public {
+        token1.transfer(address(pair), 1 ether);
+        token2.transfer(address(pair), 1 ether);
+        pair.mint();
+        assertEq(pair.balanceOf(address(this)), 1 ether - MINIMUM_LIQUIDITY);
+        assertEq(pair.totalSupply(), 1 ether);
+
+        token1.transfer(address(pair), 2 ether);
+        token2.transfer(address(pair), 1 ether);
+        pair.mint();
+        assertEq(pair.balanceOf(address(this)), 2 ether - MINIMUM_LIQUIDITY);
+        assertReserves(3 ether, 2 ether);
+        assertEq(pair.totalSupply(), 2 ether);
+    }
+
+    function testBurn() public {
+        token1.transfer(address(pair), 10 ether);
+        token2.transfer(address(pair), 10 ether);
+        pair.mint();
+        assertEq(pair.balanceOf(address(this)), 10 ether - MINIMUM_LIQUIDITY);
+        assertReserves(10 ether, 10 ether);
+        assertEq(pair.totalSupply(), 10 ether);
+
+        pair.transfer(address(pair), 5 ether);
+        pair.burn();
+        assertEq(pair.balanceOf(address(this)), 5 ether - MINIMUM_LIQUIDITY);
+        assertReserves(5 ether, 5 ether);
+        assertEq(token1.balanceOf(address(this)), 5 ether);
+        assertEq(token2.balanceOf(address(this)), 5 ether);
+        assertEq(pair.totalSupply(), 5 ether);
+    }
+
+    function _calcExpectedOut(uint112 reserve0, uint112 reserve1, uint256 inputAmount) private pure returns (uint256) {
+        uint256 _reserve0 = uint256(reserve0);
+        uint256 _reserve1 = uint256(reserve1);
+        return _reserve1 * inputAmount / (_reserve0 + inputAmount);
+    }
+
+    function testSwap() public {
+        token1.transfer(address(pair), 10 ether);
+        token2.transfer(address(pair), 10 ether);
+        pair.mint();
+        assertEq(pair.balanceOf(address(this)), 10 ether - MINIMUM_LIQUIDITY);
+        assertReserves(10 ether, 10 ether);
+        assertEq(pair.totalSupply(), 10 ether);
+
+        uint256 balance1Before = token1.balanceOf(address(this));
+        uint256 balance2Before = token2.balanceOf(address(this));
+
+        (uint112 _reserve0, uint112 _reserve1) = pair.getReserves();
+
+        uint256 expectedOut = _calcExpectedOut(_reserve0, _reserve1, 1 ether);
+
+        token1.transfer(address(pair), 1 ether);
+        vm.expectEmit(true, true, false, true, address(pair));
+        emit Swap(address(this), 0, expectedOut, address(this));
+        pair.swap(0, expectedOut, address(this));
+        console.log(balance1Before - token1.balanceOf(address(this)));
+        console.log(token2.balanceOf(address(this)) - balance2Before);
+    }
+}
